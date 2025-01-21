9f4711609575e03736c5ef84fec73a0776b0d9e6
SmartCoderAbhishek <patil.abhishek.66333@outlook.com>
2025-01-21 23:53:46 +0530

    New controller created

---
+++ akmsservice/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace akmsservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(ILogger<AccountController> logger) : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger = logger;
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            await Task.Delay(100);
+            _logger.LogInformation("GetUsers called");
+            return Ok("GetUsers called");
+        }
+    }
+}
